7c5953dba6320e2e5a58883105a6b2ef8fa566d8
AnitaAkter <108604188+AnitaAkter@users.noreply.github.com>
2022-11-16 22:52:07 +0600

    added

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import SBlog from './SBlog';
 
 const Blog = () => {
+    const [blogs, setBlogs] = useState([])
+    useEffect(() => {
+        fetch('https://newserver-anitaakter.vercel.app/blogs')
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    }, [])
     return (
         <div>
-            <h1>hi</h1>
+            <div className="dark:bg-gray-800 dark:text-gray-100">
+                {
+                    blogs.map(blog => <SBlog key={blog._id} blog={blog}></SBlog>)
+                }
+            </div>
         </div>
     );
 };
